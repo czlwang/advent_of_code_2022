158475fcfd6601222646b8b70e866cf257392e88
Christopher Wang <czw@mit.edu>
2022-12-06 19:09:05 -0500

    day2 init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::env;
 mod day1;
+mod day2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}",args);
     let day: i32 = args[1].parse().unwrap();
     match day{
         1 => day1::solve(),
+        2 => day2::solve(),
         _ => println!("Invalid day"),
     }
 }
