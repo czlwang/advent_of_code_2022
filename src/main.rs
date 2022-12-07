ec7370fdc58b838364a549ce8d08ffdcfdcabc35
Christopher Wang <czw@mit.edu>
2022-12-06 19:07:20 -0500

    switch statement per day

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use std::env;
 mod day1;
 
 fn main() {
-    day1::solve();
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}",args);
+    let day: i32 = args[1].parse().unwrap();
+    match day{
+        1 => day1::solve(),
+        _ => println!("Invalid day"),
+    }
 }
