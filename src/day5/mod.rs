43ed4a22d6aa6d02a744a405157128da7e69b4f7
Christopher Wang <czw@mit.edu>
2022-12-11 03:57:35 -0500

    parse input

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,15 +1,16 @@
 use std::collections::HashMap;
 use std::fs;
-use std::io::BufReader;
-use std::io::BufRead;
 use std::path::Path;
+
+type Move = (i32, i32, i32);
 
 fn parse_input(fname: &str){
     let root_path = Path::new("/home/czw/Documents/2022/advent_of_code_2022/src/day5");
     let fpath = root_path.join(fname);
     let string = fs::read_to_string(fpath).expect("no file");
-    let mut crates_instrs = string.split("\n\n");
-    let (crates, instrs) = (crates_instrs.next().unwrap(), crates_instrs.next().unwrap());
+    let mut crates_moves = string.split("\n\n");
+    let (crates, moves) = (crates_moves.next().unwrap(), crates_moves.next().unwrap());
+
     let mut crate_map: HashMap<usize, Vec<char>> = HashMap::new();
     for line in crates.split('\n').rev().skip(1){
         let chunks = line.chars()
@@ -23,16 +24,23 @@ fn parse_input(fname: &str){
                 crate_map.get_mut(&(i+1)).unwrap().push(c[1]);
             }
         }
-        println!();
     }
-    println!("{:?}", crate_map);
-    println!("hello");
-    println!("{:?}", instrs);
-    //let file = fs::File::open(fpath).expect("no file");
-    //let lines = BufReader::new(file).read_to_string();
-                                    //.map(|l| l.unwrap());
 
+    let parsed_moves = moves.trim()
+                            .split('\n')
+                            .map(|m| 
+                                 {
+                                   let mut m_split = m.split(' ')
+                                                      .filter_map(|s| s.parse::<i32>().ok());
+                                   (m_split.next().unwrap(), m_split.next().unwrap(), m_split.next().unwrap())
+                                 }
+                                 )
+                            .collect::<Vec<_>>();
+
+    println!("{:?}", crate_map);
+    println!("{:?}", parsed_moves);
 }
+
 pub fn solve(){
     println!("{:?}", parse_input("test1.txt"));
 }
