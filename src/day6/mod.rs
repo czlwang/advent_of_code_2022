10d89c4dd05a71d7ae169bbbafc5e14b64c8b905
Christopher Wang <czw@mit.edu>
2022-12-22 21:15:52 -0500

    solve

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::path::Path;
+use std::collections::HashSet;
 
 pub fn parse_input(fname: &str) -> String{
     let root_path = Path::new("/home/czw/Documents/2022/advent_of_code_2022/src/day6");
@@ -7,6 +8,23 @@ pub fn parse_input(fname: &str) -> String{
     fs::read_to_string(fpath).expect("couldn't read file")
 }
 
+pub fn solve_brute_force(input: &str, window_size: usize) -> usize{
+    input.chars()
+         .collect::<Vec<_>>()
+         .windows(window_size)
+         .enumerate()
+         .find(|(_i,x)|{ 
+                let set: HashSet<_> = x.iter().cloned().collect();
+                set.len()==window_size
+         })
+        .expect("couldn't find marker")
+        .0
+        + window_size
+}
+
 pub fn solve(){
-    println!("{:?}",parse_input("test1.txt"));
+    assert_eq!(5,solve_brute_force(&parse_input("test1.txt"), 4));
+    assert_eq!(6,solve_brute_force(&parse_input("test2.txt"), 4));
+    println!("{:?}",solve_brute_force(&parse_input("input1.txt"), 4));
+    println!("{:?}",solve_brute_force(&parse_input("input1.txt"), 14));
 }
