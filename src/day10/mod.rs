c3512125ee569baa9e98f6827d09221e28e05321
Christopher Wang <czw@mit.edu>
2022-12-25 19:18:53 -0500

    solve 1

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -28,38 +28,33 @@ fn parse_input(fname: &str) -> Vec<Op>{
 }
 
 fn execute(ops: Vec<Op>) -> Vec<i32>{
-    println!("{:?}", &ops[..21]);
     let mut history = Vec::new();
-    let max_delay = 2;
     let mut xreg = 1;
-    for i in 0..(ops.len()+max_delay){
-        for lag in 0..max_delay{
-            if (0 <= (i as i32) - (lag as i32)) && (i-lag < ops.len()){
-                let op = &ops[i-lag];
-                println!("{:?}", (op, xreg));
-                match op {
-                    Op::Noop => {if lag==0 {}},
-                    Op::Addx(x) => {if lag==1 {xreg += x}},
-                }
-            }
+    history.push(xreg);
+    for op in ops{
+        match op{
+            Op::Noop => {for _ in 0..1 {history.push(xreg);}},
+            Op::Addx(x) => {for _ in 0..1 {
+                                history.push(xreg);
+                            }
+                            xreg += x;
+                            history.push(xreg);
+                           },
         }
-        history.push(xreg);
     }
-    println!("{:?}", history);
     history
 }
 
 fn solve1(ops: Vec<Op>) -> i32{
-    //ops.into_iter()
-    //   .map(|op| match op { Op::Addx(x) => x, Op::Noop => 0 })
-    //   .sum()
     let history = execute(ops);
-    println!("{:?}", (history[19], history[20], history[21]));
-    1
-    //history[20]*history[60]*history[100]*history[140]*history[180]*history[220]
+    let mut sum = 0;
+    for idx in [20,60,100,140,180,220]{
+        sum += (idx as i32)*history[idx-1];
+    }
+    sum
 }
 
 pub fn solve(){
     assert_eq!(13140, solve1(parse_input("test1.txt")));
-    //assert_eq!(13140, solve1(parse_input("test2.txt")));
+    println!("{:?}", solve1(parse_input("input1.txt")));
 }
