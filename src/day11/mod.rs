93d49cf78386b6fde93a813c8a91148d0b29cc58
Christopher Wang <czw@mit.edu>
2022-12-31 16:37:54 -0500

    day11

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,25 +1,68 @@
 use std::fs;
 use std::path::Path;
+use std::collections::HashMap;
+use itertools::Itertools;
 
 #[derive(Debug)]
 struct Monkey {
-    id: i32,
-    items: Vec<i32>,
-    operation: String,
-    test: String,
-    tbranch: i32,
-    fbranch: i32
+    id: u128,
+    items: Vec<u128>,
+    operation: Op,
+    test: u128,
+    tbranch: u128,
+    fbranch: u128,
+    monkey_business: u128
 }
 
 impl Monkey{
-    fn new(monkey_id: i32, items_list: Vec<i32>, tbranch: i32, fbranch: i32) -> Self{
+    fn new(monkey_id: u128, items_list: Vec<u128>, op: Op, test_n: u128, tbranch: u128, fbranch: u128) -> Self{
         Self {id: monkey_id,
               items: items_list,
-              operation: "a".to_string(),
-              test: "b".to_string(),
+              operation: op,
+              test: test_n,
               tbranch,
-              fbranch}
+              fbranch,
+              monkey_business: 0}
     }
+}
+
+#[derive(Debug)]
+struct Op {
+    args: Vec<String>,
+    opcode: String
+}
+
+impl Op {
+    fn new(args: Vec<String>, opcode: String) -> Self{
+        Self {args, opcode}
+    }
+
+    fn execute(&self, lookup: HashMap<&str, u128>) -> u128{
+        let values = self.args.iter()
+                              .map(|x| {
+                                        let num = x.parse::<u128>();
+                                        match num {
+                                                    Ok(x) => x,
+                                                    Err(_) => *lookup.get(&x[..])
+                                                                     .expect("lookup failed")
+                                                  }
+                                        })
+                              .collect::<Vec<_>>();
+        match self.opcode.as_str() {
+            "*" => values.iter().product(),
+            "+" => values.iter().sum(),
+            _ => panic!("opcode not found")
+        }
+    }
+}
+
+fn parse_op(op_string: &str) -> Op{
+    let tokens = op_string.split(' ').collect::<Vec<_>>();
+    let operand1 = tokens.get(5).expect("not found").to_string();
+    let opcode = tokens.get(6).expect("not found").to_string();
+    let operand2 = tokens.get(7).expect("not found").to_string();
+    let args = vec![operand1, operand2];
+    Op::new(args, opcode)
 }
 
 fn parse_monkey(monkey_string: &str) -> Monkey{
@@ -32,7 +75,7 @@ fn parse_monkey(monkey_string: &str) -> Monkey{
                                .to_string();
     id_string.pop();
 
-    let monkey_id = id_string.parse::<i32>()
+    let monkey_id = id_string.parse::<u128>()
                              .expect("can't parse id");
 
     let items_string = lines.next().expect("no line")
@@ -42,15 +85,22 @@ fn parse_monkey(monkey_string: &str) -> Monkey{
                                  .expect("no list")
                                  .split(',')
                                  .map(|x| x.trim()
-                                           .parse::<i32>()
+                                           .parse::<u128>()
                                            .expect("can't parse"))
                                  .collect::<Vec<_>>();
 
     let operation_string = lines.next()
                                 .expect("no line");
+    let op = parse_op(operation_string);
 
     let test_string = lines.next()
                            .expect("no line");
+    let test_n = test_string.split(' ')
+                            .collect::<Vec<_>>()
+                            .get(5)
+                            .expect("no number")
+                            .parse::<u128>()
+                            .expect("couldn't parse");
 
     let tbranch_string = lines.next()
                               .expect("no line");
@@ -59,7 +109,7 @@ fn parse_monkey(monkey_string: &str) -> Monkey{
                                 .collect::<Vec<_>>()
                                 .get(9)
                                 .expect("no number")
-                                .parse::<i32>()
+                                .parse::<u128>()
                                 .expect("couldn't parse");
 
 
@@ -70,23 +120,82 @@ fn parse_monkey(monkey_string: &str) -> Monkey{
                                 .collect::<Vec<_>>()
                                 .get(9)
                                 .expect("no number")
-                                .parse::<i32>()
+                                .parse::<u128>()
                                 .expect("couldn't parse");
 
-    Monkey::new(monkey_id, items_list, tbranch, fbranch)
+    Monkey::new(monkey_id, items_list, op, test_n, tbranch, fbranch)
 }
 
-fn parse_input(fname: &str){
+fn turn(monkey: &mut Monkey, divide_by: u128, n: u128) -> HashMap<u128, Vec<u128>>{
+    let items = &monkey.items;
+    let mut moves: HashMap<u128, Vec<u128>> = HashMap::new();
+    moves.insert(monkey.tbranch, Vec::new());
+    moves.insert(monkey.fbranch, Vec::new());
+
+    for item in items{
+        let args = HashMap::from([("old", *item)]);
+        let mut new_item = monkey.operation.execute(args);
+        new_item /= divide_by;
+        new_item %= n;
+        if new_item % monkey.test == 0 {
+            moves.get_mut(&monkey.tbranch).expect("no monkey").push(new_item);
+        }else {
+            moves.get_mut(&monkey.fbranch).expect("no monkey").push(new_item);
+        }
+        monkey.monkey_business += 1;
+    }
+    monkey.items = Vec::new();
+    moves
+}
+
+fn round(monkeys: &mut Vec<Monkey>, divide_by: u128, n: u128){
+    for i in 0..monkeys.len(){
+        let monkey = monkeys.get_mut(i)
+                            .expect("couldn't find monkey");
+        let moves = turn(monkey, divide_by, n);
+        for key in moves.keys(){
+            let move_items = moves.get(key)
+                                  .expect("couldn't find monkey");
+            monkeys[(*key as usize)]
+                   .items
+                   .extend(move_items);
+        }
+    }
+}
+
+fn run_rounds(mut monkeys: Vec<Monkey>, num_rounds: i32, divide_by: u128) -> u128{
+    let n = monkeys.iter().map(|m| m.test).product::<u128>();
+    for _ in 0..num_rounds {
+        round(&mut monkeys, divide_by, n);
+    }
+    monkeys.iter()
+           .map(|m| m.monkey_business)
+           .sorted()
+           .rev()
+           .take(2)
+           .product()
+}
+
+fn solve1(monkeys: Vec<Monkey>) -> u128{
+    run_rounds(monkeys, 20, 3)
+}
+
+fn solve2(mut monkeys: Vec<Monkey>) -> u128{
+    run_rounds(monkeys, 10000, 1)
+}
+
+fn parse_input(fname: &str) -> Vec<Monkey>{
     let root_path = Path::new("/home/czw/Documents/2022/advent_of_code_2022/src/day11/");
     let fpath = root_path.join(fname);
     let string = fs::read_to_string(fpath).expect("file not found");
-    let monkeys = string.split("\n\n")
-                        .map(parse_monkey)
-                        .collect::<Vec<_>>();
-    println!("{:?}", monkeys);
+    string.split("\n\n")
+          .map(parse_monkey)
+          .collect::<Vec<_>>()
 }
 
 pub fn solve(){
-    parse_input("test1.txt");
-    println!("hello");
+    assert_eq!(10605, solve1(parse_input("test1.txt")));
+    println!("{:?}", solve1(parse_input("input1.txt")));
+    assert_eq!(2713310158, solve2(parse_input("test1.txt")));
+    println!("{:?}", solve2(parse_input("input1.txt")));
 }
